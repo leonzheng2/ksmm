5f6948ce631058a5dadba5538c88110a74cf7db7
Léon Zheng <leon.zheng1@huawei.com>
2025-03-28 16:46:16 +0000

    misc

--- scripts/3bis_benchmark_ksllama_time.sh
+++ scripts/3bis_benchmark_ksllama_time.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-algo="kernel"
-python3 src/ksmm_py/benchmark/ksllama_time.py \
-  --algo=$algo \
+CUDA_VISIBLE_DEVICES=1 python3 src/ksmm_py/benchmark/ksllama_time.py \
+  --algo="kernel" \
   --up-pattern1 1 16 32 896  \
   --up-pattern2 8 3584 512 1 \
   --down-pattern1 1 512 3584 8 \

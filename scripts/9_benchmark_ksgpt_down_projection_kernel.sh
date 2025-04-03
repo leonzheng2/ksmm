ab29eacdb664d02c8a7a200bdebc1e66f3daefae
Léon Zheng <leon.zheng1@huawei.com>
2025-04-03 08:48:08 +0000

    benchmarking ksgpt kernel vs bmm vs dense

--- scripts/9_benchmark_ksgpt_down_projection_kernel.sh
+++ scripts/9_benchmark_ksgpt_down_projection_kernel.sh
@@ -19,7 +19,7 @@ while read -r line_down; do
     echo "-----------------"
     read da1 db1 dc1 dd1 da2 db2 dc2 dd2 <<< $line_down
     echo "$da1 $db1 $dc1 $dd1 $da2 $db2 $dc2 $dd2"
-    for algo in "kernel ""bmm"
+    for algo in "kernel" "bmm"
     do
       python3 src/ksmm_py/benchmark/ksgpt_time.py \
         --model-id $model_id \

943ff29936f792dbf327dd8f234e93516a0127f7
Léon Zheng <leon.zheng1@huawei.com>
2025-04-07 13:04:38 +0000

    add .txt

--- scripts/9bis_check_one_kslinear.sh
+++ scripts/9bis_check_one_kslinear.sh
@@ -1,13 +1,11 @@
 
 # gpt2-large
 model_id="gpt2-medium"
-list_chains="../../../results/0_ksmm/processed/interesting-gpt-1024x4096.txt"
-saving_dir="../../../results/3_gpt/9bis_check_one_kslinear"
-
-cd "src/ksmm_py/benchmark"
+list_chains="results/0_ksmm/processed/interesting-gpt-1024x4096.txt"
+saving_dir="results/3_gpt/9bis_check_one_kslinear"
 
 # dense
-python ksmm_time.py \
+python src/ksmm_py/benchmark/ksmm_time.py \
     --patterns "1,1024,4096,1" \
     --batch-size 25088 \
     --precision "fp32" \
@@ -23,7 +21,7 @@ while read -r line_down; do
     echo "$da1 $db1 $dc1 $dd1 $da2 $db2 $dc2 $dd2"
     for algo in "kernel" "bmm"
     do
-      python ksmm_time.py \
+      python src/ksmm_py/benchmark/ksmm_time.py \
         --patterns "${da2},${db2},${dc2},${dd2}" "${da1},${db1},${dc1},${dd1}" \
         --batch-size 25088 \
         --precision "fp32" \
@@ -33,5 +31,3 @@ while read -r line_down; do
         --saving-csv "${saving_dir}/algo=${algo}-${da2},${db2},${dc2},${dd2}-${da1},${db1},${dc1},${dd1}.csv"
     done
 done < $list_chains
-
-cd -
